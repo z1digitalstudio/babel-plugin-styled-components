81ca77d620c6025abe0d6fab1030aa44f6e8b0e4
Pablo Rodríguez Caballero <pablo@rodriguezcaballero.me>
2020-09-23 11:05:42 +0200

    Add "meaninglessFileNames" option
    The "meaninglessFileNames" option provides the user with more precise
    control over the component display name.
    
    This option is to be used in conjunction with "displayName" and
    "fileName". If either is set to false, this option will have no effect.
    
    Prior to this commit, when both "displayName" and "fileName" were set,
    the behaviour was to prepend the file name to the component display name
    if it was named anything other than "index", and to prepend the
    directory name otherwise.
    
    The "meaninglessFileNames" option enables developers to control when
    exactly the directory name should be used instead of the file name. If
    the file name is considered to be "meaningless", that is, it doesn't
    provide the developer with any useful information, then the directory
    name will be used instead.
    
    By default, the only "meaningless file name" is "index", which means
    that the default behaviour is unmodified.

--- src/utils/options.js
+++ src/utils/options.js
@@ -9,6 +9,7 @@ export const useTopLevelImportPaths = state =>
   getOption(state, 'topLevelImportPaths', [])
 export const useSSR = state => getOption(state, 'ssr', true)
 export const useFileName = state => getOption(state, 'fileName')
+export const useMeaninglessFileNames = state => getOption(state, 'meaninglessFileNames', ['index'])
 export const useMinify = state => getOption(state, 'minify')
 export const useTranspileTemplateLiterals = state =>
   getOption(state, 'transpileTemplateLiterals')
